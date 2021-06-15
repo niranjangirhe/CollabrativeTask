8b5ab86bb176f3ffe6ca3f4b9ab0dd33a6dace6a
niranjangirhe <niranjangirheindia@gmail.com>
2021-06-15 12:57:51 +0530

    added filehanding

--- app/src/main/java/com/ngsolutions/collabrativetask/NewTaskAcitvity.java
+++ app/src/main/java/com/ngsolutions/collabrativetask/NewTaskAcitvity.java
@@ -2,13 +2,94 @@ package com.ngsolutions.collabrativetask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class NewTaskAcitvity extends AppCompatActivity {
 
+    ImageButton addTask;
+    EditText taskName;
+    private static final String FILE_NAME = "myTasks.txt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task_acitvity);
+        addTask = findViewById(R.id.AddTaskFromNormal);
+        taskName = findViewById(R.id.TaskNameFromAddNormal);
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(taskName.getText().toString().isEmpty())
+                {
+                    Toast.makeText(NewTaskAcitvity.this, "Please Enter Name", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    Toast.makeText(NewTaskAcitvity.this, "Task Added Successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void save() {
+        String text = taskName.getText().toString();
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            taskName.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void load() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+            taskName.setText(sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
